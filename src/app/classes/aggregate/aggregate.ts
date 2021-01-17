1711159e51c4a1b0ecf44ed8815af07da052fdc7
jayzver <phonem5note@gmail.com>
2021-01-17 16:03:03 +0200

    gggg

---
+++ src/app/classes/aggregate/aggregate.ts
@@ -0,0 +1,12 @@
+export class Aggregate
+{
+  id: number;
+  nameTarget: string;
+  parentId: number;
+  groupId: number;
+  quantity: number;
+  imgUrl: string;
+  description: string;
+  serialKey: string;
+
+}
---
+++ src/app/classes/aggregate/provider.ts
@@ -0,0 +1,11 @@
+export class Provider
+{
+  id: number;
+  nameProvider: string;
+  imgUrl: string;
+  description: string;
+  internAddress: string;
+  physicalAddress: string;
+  email: string;
+  phone: string;
+}
